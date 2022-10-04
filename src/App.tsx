53e61c6632fa3fc9e6854335e003195afecc9395
kalz2q <katsu95i@gmail.com>
2022-10-04 18:50:00 +0900

    M

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {useState} from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
